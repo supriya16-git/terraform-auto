1f24b6dca0340ecd525470f607c44266dd00c771
supriya16-git <supriyasoft16@gmail.com>
2025-03-29 00:30:37 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "cherry_instance" {
 
 # Create S3 bucket
 resource "aws_s3_bucket" "cherry_bucket" {
-  bucket = "20th-cherry-bucket"
+  bucket = "21st-cherry-bucket"
   lifecycle {
     ignore_changes = [bucket]
   }
